8a6bb2e902b1849734a2fa5826dfd480768d56ad
Nadar <nadaralp16@gmail.com>
2019-06-24 19:16:31 +0300

    generating shades of colors with color helper

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Palette from './Palette';
 import seedColors from '../seedColor';
+import generatePalette from '../utils/colorHelpers';
 
 function App() {
+  console.log(generatePalette(seedColors[2]))
   return (
     <div >
       <Palette {...seedColors[4]} />

37bf9428d7b1904e5a2c3b69963341d4361b9e3b
Nadar <nadaralp16@gmail.com>
2019-06-25 16:51:27 +0300

    find palette and render by param id

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import Palette from './Palette';
 import seedColors from '../seedColor';
 import generatePalette from '../utils/colorHelpers';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
+
+  const findPalette = id => seedColors.find(palette => palette.id === id)
+
+
   return (
-    <div >
-      <Palette palette={generatePalette(seedColors[2])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+      <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))} />} />
+    </Switch>
+    // <div >
+    //   <Palette palette={generatePalette(seedColors[2])} />
+    // </div>
   );
 }
 

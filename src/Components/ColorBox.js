9ee80217c18aaa2fba4566cfec4f9afc30d0a9a3
Nadar <nadaralp16@gmail.com>
2019-06-26 15:39:31 +0300

    Refactored styles to use dynamic JSS styling

--- src/Components/ColorBox.js
+++ src/Components/ColorBox.js
@@ -2,11 +2,54 @@ import React, { Component } from 'react';
 import '../styles/Colorbox.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
-import { styled } from '@material-ui/styles';
+import { withStyles, styled } from '@material-ui/styles';
 import chroma from 'chroma-js';
 
+const styles = {
+    darkenText: {
+        color: props => chroma(props.background).luminance() >= 0.55 ? "rgba(0, 0, 0, 0.9)" : "white"
+    },
+    seeMore: {
+        position: 'absolute',
+        background: props => chroma(props.background).luminance() >= 0.55 ? 'rgba(0, 0, 0, 0.1)' : 'rgba(255, 255, 255, 0.3)',
+        border: 'none',
+        right: '0px',
+        bottom: '0px',
+        color: props => chroma(props.background).luminance() >= 0.55 ? "rgba(0, 0, 0, 0.9)" : "white",
+        width: '60px',
+        height: '30px',
+        textAlign: 'center',
+        lineHeight: '30px',
+        textTransform: 'uppercase'
+    },
+    copyButton: {
+        width: '100px',
+        height: '30px',
+        position: 'absolute',
+        display: 'inline-block',
+        top: '50%',
+        left: '50%',
+        marginLeft: '-50px',
+        marginTop: '-15px',
+        textAlign: 'center',
+        outline: 'none',
+        background: props => chroma(props.background).luminance() >= 0.55 ? 'rgba(0, 0, 0, 0.1)' : 'rgba(255, 255, 255, 0.3)',
+        fontSize: '1rem',
+        lineHeight: '30px',
+        color: props => chroma(props.background).luminance() >= 0.55 ? "rgba(0, 0, 0, 0.9)" : "white",
+        textTransform: 'uppercase',
+        border: 'none',
+        opacity: '0',
+        cursor: 'pointer',
+        ".ColorBox:hover &": {
+            opacity: '1',
+            transition: '0.5s',
+        }
+    }
+}
 
-export default class ColorBox extends Component {
+
+class ColorBox extends Component {
 
     constructor(props) {
         super(props);
@@ -29,7 +72,7 @@ export default class ColorBox extends Component {
     };
 
     render() {
-        const { name, background, paletteId, id, renderer } = this.props;
+        const { name, background, paletteId, id, renderer, classes } = this.props;
         const { copied } = this.state;
         const StyledLink = styled(Link)({
             textDecoration: 'none',
@@ -39,25 +82,24 @@ export default class ColorBox extends Component {
             }
         });
 
-        const isDarkColor = chroma(background).luminance() <= 0.1;
-        const isLightColor = chroma(background).luminance() >= 0.6;
-
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className="ColorBox">
                     <div className={`copy-overlay ${copied && 'show'}`} style={{ background }}></div>
-                    <div className={`copy-message ${copied && 'show'} ${isLightColor && 'dark-text'}`}><h1>Copied</h1><p>{background}</p></div>
+                    <div className={`copy-message ${copied && 'show'}`}><h1>Copied</h1><p className={classes.darkenText}>{background}</p></div>
                     <div className="copy-container">
                         <div className="box-content">
-                            <span className={isDarkColor && 'light-text'}>{name}</span>
+                            <span className={classes.darkenText}>{name}</span>
                         </div>
-                        <button className={`copy-button ${isLightColor && "dark-text"}`}>Copy</button>
+                        <button className={classes.copyButton}>Copy</button>
                     </div>
                     {
-                        this.props.showLink && <span className={`see-more ${isLightColor && "dark-text"}`}> <StyledLink onClick={e => e.stopPropagation()} to={`/palette/${paletteId}/${id}`} >More</StyledLink></span>
+                        this.props.showLink && <span className={classes.seeMore}> <StyledLink onClick={e => e.stopPropagation()} to={`/palette/${paletteId}/${id}`} >More</StyledLink></span>
                     }
                 </div>
             </CopyToClipboard>
         )
     }
 }
+
+export default withStyles(styles)(ColorBox);
